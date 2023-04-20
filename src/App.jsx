7804061e2ee8e6307a2bcc1251a13ec1af1ad4f2
cgabrielbg7 <A01284477@itesm.mx>
2023-04-20 09:54:50 -0600

    Campo de Usuario y Contraseña

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import Navbar from "./components/Navbar";
+import WithoutNav from "./components/WithoutNav";
+import Log from "./pages/Log";
+import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ShoppingList from "./pages/ShoppingList";
 import Products from "./pages/Products";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import WithNav from "./components/WithNav";
 
 const App = () => {
-
+  /*const [currentForm, setCurrentForm] = useState('Log')*/
   return (
     <div>
         <Router>
           <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/edit-shopping-list" exact element={<Products />} />
-            <Route path="/shopping-list" exact element={<ShoppingList />} />
-          </Routes>
-          <Navbar />
+            <Route element={<WithoutNav />}>
+              <Route path="/log" exact element={<Log/>}/>
+              <Route path="/register" exact element={<Register/>}/>
+            </Route>
+            <Route element={<WithNav />}>
+              <Route path="/home" exact element={<Home />} />
+              <Route path="/edit-shopping-list" exact element={<Products />} />
+              <Route path="/shopping-list" exact element={<ShoppingList />} />
+            </Route>
+         </Routes>
         </Router>
     </div>
   )
