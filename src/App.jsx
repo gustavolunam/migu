74bcbe38c8d0419e51c614032643e02b4f9b5cff
Max Jauregui <a01570673@tec.mx>
2023-04-20 12:13:01 -0600

    Start Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Navbar from "./components/Navbar";
+import WithNav from "./components/WithNav";
 import WithoutNav from "./components/WithoutNav";
+import Start from "./pages/Start";
 import Log from "./pages/Log";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ShoppingList from "./pages/ShoppingList";
 import Products from "./pages/Products";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import WithNav from "./components/WithNav";
+
 
 const App = () => {
   /*const [currentForm, setCurrentForm] = useState('Log')*/
@@ -15,6 +16,7 @@ const App = () => {
         <Router>
           <Routes>
             <Route element={<WithoutNav />}>
+              <Route path="/" exact element={<Start/>}/>
               <Route path="/log" exact element={<Log/>}/>
               <Route path="/register" exact element={<Register/>}/>
             </Route>
@@ -23,7 +25,7 @@ const App = () => {
               <Route path="/edit-shopping-list" exact element={<Products />} />
               <Route path="/shopping-list" exact element={<ShoppingList />} />
             </Route>
-         </Routes>
+          </Routes>
         </Router>
     </div>
   )
