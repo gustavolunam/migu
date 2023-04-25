b838bf24d59fa328e10bf0c58be6d91798e9edbe
Max Jauregui <a01570673@tec.mx>
2023-04-25 12:02:49 -0600

    Navbar sticky
    La barra de navegacion se mantiene fija en el pie de pagina de la pantalla

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import '../styles/Auth.css'
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { getDatabase, ref, onValue, set, push, update } from "firebase/database";
