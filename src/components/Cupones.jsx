568554403fde4233bc4811513c2d9adfcba19208
Mario Jauregui Gomez <69372172+FannarLumi@users.noreply.github.com>
2023-05-24 18:40:54 -0600

    Avance de Popups de cupones
    Ya funcionan los pop ups de los cupones

--- src/components/Cupones.jsx
+++ src/components/Cupones.jsx
@@ -1,31 +1,35 @@
 import '../styles/useCuponsDisplay.css'
 import Popup from './Popup.jsx';
-import {useState} from 'react';
+import { useState } from 'react';
 
-const Listcupons = ({items}) => {
+const Listcupons = ({ items }) => {
 
-    const [showPopUp, setShowPopUp] = useState(false); 
+    const [showPopUp, setShowPopUp] = useState(false);
+    const [nombre, setNombre] = useState("");
+    const [descripcion, setDescripcion] = useState("");
 
-    const show = () => {
-        setShowPopUp(true)
+    const show = (item) => {
+        setNombre(item.nombre);
+        setDescripcion(item.descripcion);
+        setShowPopUp(true);
     }
-    return(
+    return (
         <>
             {items.map(item => (
                 <div className='cupons'>
-                    <button className="cuponButton"  onClick = {show}>
+                    <button className="cuponButton" onClick={() => show(item)}>
                         <div className='cuponscontainer'>
                             <h2 className="cuponName">{item.nombre}</h2>
-                            <p className="cuponDate">Expira:  {item.fechaExp}</p>  
-                        </div>    
+                            <p className="cuponDate">Expira:  {item.fechaExp}</p>
+                        </div>
                     </button>
-                    <Popup trigger = {showPopUp} setTrigger = {setShowPopUp}>
-                        <h1>{item.nombre}</h1>
-                        <p>{item.descripcion}</p>
-                    </Popup>
                 </div>
             ))}
-            <div className = "cuponFooter"/>
+            <Popup trigger={showPopUp} setTrigger={setShowPopUp} >
+                <h1>{nombre}</h1>
+                <p>{descripcion}</p>
+            </Popup >
+            <div className="cuponFooter" />
         </>
     )
 }
