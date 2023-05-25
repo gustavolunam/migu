8629bedb43b33835fdeee5a1f0ffa5066830e26e
Mario Jauregui Gomez <69372172+FannarLumi@users.noreply.github.com>
2023-05-25 08:58:19 -0600

    PopUps de Cupones
    La UI de los cupones ya quedó para poder usarlos

--- src/components/Cupones.jsx
+++ src/components/Cupones.jsx
@@ -7,10 +7,12 @@ const Listcupons = ({ items }) => {
     const [showPopUp, setShowPopUp] = useState(false);
     const [nombre, setNombre] = useState("");
     const [descripcion, setDescripcion] = useState("");
+    const [fechaExp, setFechaExp] = useState("");
 
     const show = (item) => {
         setNombre(item.nombre);
         setDescripcion(item.descripcion);
+        setFechaExp(item.fechaExp);
         setShowPopUp(true);
     }
     return (
@@ -26,8 +28,14 @@ const Listcupons = ({ items }) => {
                 </div>
             ))}
             <Popup trigger={showPopUp} setTrigger={setShowPopUp} >
-                <h1>{nombre}</h1>
-                <p>{descripcion}</p>
+                <div className = "popUpBorder">
+                    <h1>{nombre}</h1>
+                    <p>{descripcion}</p>
+                    <h2>Expira: {fechaExp}</h2>
+                    <button class="popUpbtn">
+                        <h3>USAR</h3>
+                    </button>
+                </div>
             </Popup >
             <div className="cuponFooter" />
         </>
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import '../styles/PopUp.css'
+import '../styles/PopUp.css';
 
 function Popup(items) {
   return (items.trigger) ? (
     <div className = "popup">
         <div className = "popupInner">
-            <button className="closeButton" onClick={() => items.setTrigger(false)}>Close</button>
+            <button className="closeButton" onClick={() => items.setTrigger(false)}>
+              X
+            </button>
             {items.children}
+            
         </div>
     </div>
   ) : "";
