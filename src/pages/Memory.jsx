baf7d33dc3626a70b367f9ca96c66c9d81855082
Gabriel Bazan <a01284477@tec.mx>
2023-05-28 11:08:20 -0600

    Memory

--- src/pages/Memory.jsx
+++ src/pages/Memory.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import { auth } from "../apis/firebaseConfig"
+import { onAuthStateChanged } from "firebase/auth";
 import Board from '../components/Memory/Board/Board';
 const emojiList = [...'🍕🎮🎱💩⚽🥅'];
 
@@ -7,6 +10,7 @@ const Memory = () => {
   const [selectedMemoBlock, setselectedMemoBlock] = useState(null);
   const [animating, setAnimating] = useState(false);
   const [gameWon, setGameWon] = useState(false);
+  const [products, setCupons] = useState([]);
 
   useEffect( () => {
     const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
@@ -47,17 +51,56 @@ const Memory = () => {
     }
   }
 
+  const getRandomNum = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  const getCupon = () => {
+    let random = getRandomNum(1,10);
+    console.log(random);
+    const db=getDatabase();
+    const dbRef = ref(db, `Cupones/${random}`);
+    //let fetCupons=[];
+
+    onValue(dbRef, (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            const nombre=snapshot.child("nombre").val();
+            const desc=snapshot.child("descripcion").val();
+            const fechaExp=snapshot.child("fechaExp").val();
+            const fechaIni=snapshot.child("fechaIni").val();
+            const aplicado=snapshot.child("aplicado").val();
+            
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    const uid = user.uid;
+                    const dbRef1 = ref(db,`Usuarios/${uid}/ListaCupones/${random}`);
+                    set(dbRef1,{
+                        nombre: nombre,
+                        descripcion: desc,
+                        fechaExp: fechaExp,
+                        fechaIni: fechaIni,
+                        aplicado: aplicado
+                    });
+                }
+            });
+        });
+      });
+}
+
   return (
     <>
       <Board memoBlocks={shuffledMemoBlocks} animating={animating} handleMemoClick={handleMemoClick} />
       {gameWon && (
         <div className="popup">
           <h2>FELICIDADES</h2>
+          <button type="button" onClick={getCupon}>Click Me</button>
         </div>
       )}
     </>
   );
-  
+
 }
 
 export default Memory;
